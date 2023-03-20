c429a5c57a01670d6c801ab409892f15b9949900
Bartlomiej Snochowski <bartlomiej.snochowski@gmail.com>
2023-03-20 12:38:37 +0100

    index.js setting

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import App from './base/App';
+import store from './base/store';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
