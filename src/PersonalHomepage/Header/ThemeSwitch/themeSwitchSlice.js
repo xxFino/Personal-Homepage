c72e0df6e46e7b80655b46a414f8691aa40930fc
Bartlomiej Snochowski <bartlomiej.snochowski@gmail.com>
2023-03-21 11:12:41 +0100

    change in themeSwitchSlice

--- src/PersonalHomepage/Header/ThemeSwitch/themeSwitchSlice.js
+++ src/PersonalHomepage/Header/ThemeSwitch/themeSwitchSlice.js
@@ -13,7 +13,7 @@ const themeSwitchSlice = createSlice({
 });
 
 export const { toggleTheme } = themeSwitchSlice.actions;
-const selectThemeState = state => state.theme;
+const selectThemeState = state => state.themeSwitch;
 export const selectIsDarkTheme = state => selectThemeState(state).darkModeOff;
 
 export default themeSwitchSlice.reducer;
