3b100d9f84d4e5e905f2a56d8c63a84b2435fbff
Bartlomiej Snochowski <bartlomiej.snochowski@gmail.com>
2023-03-22 01:41:21 +0100

    code program responsible for theme switch

--- src/PersonalHomepage/Header/ThemeSwitch/index.js
+++ src/PersonalHomepage/Header/ThemeSwitch/index.js
@@ -1,15 +1,25 @@
 import { useDispatch, useSelector } from "react-redux";
-import { Text, ThemeButton } from "./styled";
-import {  selectIsDarkTheme, toggleTheme } from "./themeSwitchSlice";
+import { IconWrapper, Text, ThemeButton, Toogle, Wrapper } from "./styled";
+import { selectIsDarkTheme, toggleTheme } from "./themeSwitchSlice";
+import SunLight from "./images/SunLight.svg";
+import SunDark from "./images/SunDark.svg";
 
 const ThemeSwitch = () => {
     const isDarkTheme = useSelector(selectIsDarkTheme);
     const dispatch = useDispatch();
 
     return (
-       <ThemeButton onClick={() => dispatch(toggleTheme())}>
-            <Text>DARK MODE {isDarkTheme ? "ON" : "OFF"}</Text>
-       </ThemeButton>
+        <Wrapper>
+            <ThemeButton onClick={() => dispatch(toggleTheme())}>
+                <Text>DARK MODE {isDarkTheme ? "ON" : "OFF"}</Text>
+                <IconWrapper>
+                    <Toogle
+                        src={isDarkTheme ? SunDark : SunLight}
+                        moveRight={isDarkTheme}>
+                    </Toogle>
+                </IconWrapper>
+            </ThemeButton>
+        </Wrapper>
     )
 };
 
