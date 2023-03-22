23ab90ec43dd7858386f0a65219714cdf090c5a3
Bartlomiej Snochowski <bartlomiej.snochowski@gmail.com>
2023-03-22 14:07:34 +0100

    New description in footer

--- src/PersonalHomepage/Footer/index.js
+++ src/PersonalHomepage/Footer/index.js
@@ -12,7 +12,7 @@ const Footer = () => (
             bartlomiej.snochowski@gmail.com
         </EmailText>
         <Description>
-            I'm always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come to life, feel free to conatct me
+        Hello! I am open to new cooperation and learning new things. If you want to contact me, go ahead!
         </Description>
         <IconsContainter>
             <Link href="https://github.com/xxFino" target="_blank" title="Github">
