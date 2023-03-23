25cab742d36ee122caf63e0c46c051caf93d5743
Bartlomiej Snochowski <bartlomiej.snochowski@gmail.com>
2023-03-23 01:18:08 +0100

    removed unnecessary "return"

--- src/PersonalHomepage/Skills/index.js
+++ src/PersonalHomepage/Skills/index.js
@@ -1,20 +1,17 @@
 import { List, ListItems, SkillsPoints, SkillsTitle, Wrapper } from "./styled";
 
-const Skills = ({ title, skills }) => {
-
-    return (
-        <Wrapper>
-            <SkillsTitle>{title}</SkillsTitle>
-            <List>
-                {skills.map(skill => (
-                    <ListItems key={skill}>
-                        <SkillsPoints  />                        
-                        {skill}
-                    </ListItems>
-                ))}
-            </List>
-        </Wrapper>
-    )
-}
+const Skills = ({ title, skills }) => (
+    <Wrapper>
+        <SkillsTitle>{title}</SkillsTitle>
+        <List>
+            {skills.map(skill => (
+                <ListItems key={skill}>
+                    <SkillsPoints />
+                    {skill}
+                </ListItems>
+            ))}
+        </List>
+    </Wrapper>
+);
 
 export default Skills;
